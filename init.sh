ecdf41f15c82ce6fc35c0016e4bcf8595bdad204
Mona Lisa <vukubrian@gmail.com>
2022-07-05 01:16:06 +0300

    init

---
+++ init.sh
@@ -0,0 +1,17 @@
+yes|sudo dnf remove gnome-boxes
+yes|sudo dnf remove libreoffice*
+yes|sudo dnf remove totem
+yes|sudo dnf remove mediawriter
+yes|sudo dnf remove rhythmbox
+yes|sudo dnf remove gnome-photos
+yes|sudo dnf remove gnome-maps
+yes|sudo dnf remove gnome-tour
+yes|sudo dnf remove gnome-text-editor
+yes|sudo dnf remove cheese
+yes|sudo dnf remove gnome-connections
+yes|sudo dnf remove gnome-contacts
+yes|sudo dnf remove gnome-help
+yes|sudo dnf remove gnome-weather
+yes|dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+yes|dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
+yes|sudo dnf install vlc
