8c1916cd8446cffb6c07716d9f9a9adf459c1fd3
brayo <vukubrian@gmail.com>
2022-07-10 00:54:45 +0300

    Autonomy

--- init.sh
+++ init.sh
@@ -12,9 +12,11 @@ yes|sudo dnf remove gnome-connections
 yes|sudo dnf remove gnome-contacts
 yes|sudo dnf remove gnome-help
 yes|sudo dnf remove gnome-weather
-yes|dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-yes|dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
-yes|sudo dnf install vlc
+yes|sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+yes|sudo dnf install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
+yes|sudo dnf -y install vlc
 yes|sudo dnf install pip
+yes|sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+yes|sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 yes|sudo dnf install code
 yes|sudo dnf install foliate
