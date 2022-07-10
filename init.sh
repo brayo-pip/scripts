65bccf216ff1606b7a47ae20afa2e1db5d20facc
brayo <vukubrian@gmail.com>
2022-07-11 01:12:42 +0300

    typo error fixes

--- init.sh
+++ init.sh
@@ -19,8 +19,8 @@ git config --global user.name brayo
 git config --global user.email vukubrian@gmail.com
 
 timedatectl set-timezone Africa/Nairobi
-sudo dnf -y -q install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-qelease-$(rpm -E %fedora).noarch.rpm
-sudo dnf -y -q install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-qelease-qawhide.noarch.rpm
+sudo dnf -y -q install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y -q install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-qawhide.noarch.rpm
 sudo dnf -y -q install vlc
 sudo dnf -y -q install foliate
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
