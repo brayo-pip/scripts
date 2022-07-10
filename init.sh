3b09e875ff2f0d7de1fbc73437c2d8ebbe8aa58c
brayo <vukubrian@gmail.com>
2022-07-10 21:42:39 +0300

    remove trailing space

--- init.sh
+++ init.sh
@@ -26,4 +26,3 @@ yes|sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 yes|sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf -y install code
 sudo dnf -y install pip
-
