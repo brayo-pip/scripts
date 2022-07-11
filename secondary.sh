1a652d45c0bedcbfae25953eb5a7967ccc58818b
brayo <vukubrian@gmail.com>
2022-07-11 12:22:03 +0300

    extras

---
+++ secondary.sh
@@ -0,0 +1,10 @@
+sudo dnf -y install cronie
+sudo dnf -y install gnome-tweaks
+sudo dnf -y install golang
+git clone https://github.com/brayo-pip/xkcd-golang
+
+udo dnf -y install snapd
+sudo snap install spotify # expect device not initialized error
+sudo snap install spotify
+sudo snap install discord
+
