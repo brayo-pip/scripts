40c27ccd98d27c5333ac13254b012a5aee584d35
brayo <vukubrian@gmail.com>
2022-07-11 13:08:44 +0300

    typo fix

--- secondary.sh
+++ secondary.sh
@@ -3,7 +3,7 @@ sudo dnf -y install gnome-tweaks
 sudo dnf -y install golang
 git clone https://github.com/brayo-pip/xkcd-golang
 
-udo dnf -y install snapd
+sudo dnf -y install snapd
 sudo snap install spotify # expect device not initialized error
 sudo snap install spotify
 sudo snap install discord
