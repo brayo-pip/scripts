c15498a0e2b0b40eff3be9f95e9c30a3ce05580e
brayo <vukubrian@gmail.com>
2022-07-12 04:36:49 +0300

    add wheel

--- init.sh
+++ init.sh
@@ -27,5 +27,6 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf -y install code
 sudo dnf -y -q install pip
+pip install wheel
 
 echo 🧡✘🤖
