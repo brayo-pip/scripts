9399dc6ecaab02e77623c10a6234b77c37c2b938
brayo <vukubrian@gmail.com>
2022-07-22 13:24:09 +0300

    fix code install error

--- init.sh
+++ init.sh
@@ -27,10 +27,10 @@ sudo dnf -y -q install https://download1.rpmfusion.org/free/fedora/rpmfusion-fre
 sudo dnf -y -q install http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-rawhide.noarch.rpm
 sudo dnf -y -q install vlc
 sudo dnf -y -q install foliate
-sudo dnf -y install code
 sudo dnf -y -q install pip
 pip install wheel
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf -y install code
 
 echo 🧡✘🤖
