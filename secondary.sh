d5e632ab2fda076a8b5008df530b43abea1f39a3
brayo <vukubrian@gmail.com>
2023-04-14 02:05:14 +0300

    add spotify flatpak

--- secondary.sh
+++ secondary.sh
@@ -2,7 +2,7 @@ sudo dnf -y install golang
 git clone https://github.com/brayo-pip/xkcd-go
 
 sudo dnf -y install snapd
-sudo snap install spotify # expect device not initialized error
-sudo snap install spotify
 sudo snap install discord
 
+flatpak install flathub com.spotify.Client
+flatpak run com.spotify.Client
