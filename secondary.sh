500018d4ffb2e6f8bca531afa0405a16422f5719
brayo <vukubrian@gmail.com>
2023-04-20 13:26:40 +0300

    add symbolic link for snap

--- secondary.sh
+++ secondary.sh
@@ -2,6 +2,7 @@ sudo dnf -y install golang
 git clone https://github.com/brayo-pip/xkcd-go
 
 sudo dnf -y install snapd
+sudo ln -s /var/lib/snapd/snap /snap
 sudo snap install discord
 
 flatpak install flathub com.spotify.Client
