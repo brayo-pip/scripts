1fc47339c6ad1836bf8efd6f31c6319dae7985f5
brayo <vukubrian@gmail.com>
2024-06-07 23:12:17 +0300

    add ripgrep

--- init.sh
+++ init.sh
@@ -28,6 +28,7 @@ sudo dnf -y -q install ffmpeg --allowerasing
 sudo dnf -y -q install vlc
 sudo dnf -y -q install foliate
 sudo dnf -y -q install pip
+sudo dnf -y -q install ripgrep
 pip install wheel
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
